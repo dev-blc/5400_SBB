213c6e0931821063c1f2b83bc6557675342c644c
dev-blc <genesis.dev.blc@gmail.com>
2023-05-09 18:57:44 +0200

    <0.1> INIT & Wallet Implementation
    New Repo and created a helper file and wallet.py

---
+++ helper.py
@@ -0,0 +1,11 @@
+
+import wallet
+
+# create a new Wallet instance
+walletInstance = wallet.Wallet()
+
+# get the public key
+public_key = walletInstance.getPublicKey()
+
+print(public_key)
+
---
+++ wallet.py
@@ -0,0 +1,9 @@
+import ecdsa
+
+class Wallet:
+    def __init__(self):
+        self.sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
+        self.vk = self.sk.verifying_key
+
+    def getPublicKey(self):
+        return self.vk.to_string().hex()
